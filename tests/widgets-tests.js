59bf6ff534ea8e6e485a512e19a2ee2ceba3a627
Joel Skrepnek <jskrepnek@gmail.com>
2014-02-22 16:28:25 -0800

    the widgets service query action when invoked should return the widgets returned from the backend service

--- tests/widgets-tests.js
+++ tests/widgets-tests.js
@@ -1,6 +1,16 @@
 describe('the widgets service', function() {
 
-	var widgets;
+	var widgets,
+		$httpBackend,
+		uri = 'https://api.parse.com/1/classes/widgets';
+
+	beforeEach(function() {
+		this.addMatchers({
+			toEqualData: function(expected) {
+				return angular.equals(this.actual, expected);
+			}
+		});
+	});
 
 	beforeEach(function() {
 
@@ -8,6 +18,7 @@ describe('the widgets service', function() {
 
 		inject(function($injector) {
 			widgets = $injector.get('Widgets');
+			$httpBackend = $injector.get('$httpBackend');
 		})
 
 	});
@@ -16,11 +27,35 @@ describe('the widgets service', function() {
 		expect(widgets).toBeDefined();
 	});
 
-	describe('query action', function () {
+	describe('query action', function() {
 
-		it('should be defined', function () {
+		it('should be defined', function() {
 			expect(widgets.query).toBeDefined();
 		});
+
+		describe('when invoked', function() {
+
+			var data = [{
+				id: 1,
+				name: 'tappy'
+			}, {
+				id: 2,
+				name: 'thrappy'
+			}];
+
+			beforeEach(function() {
+				$httpBackend
+					.when('GET', uri)
+					.respond(data);
+			});
+
+			it('should return the widgets returned from the backend service', function() {
+				var result = widgets.query();
+				$httpBackend.flush();
+				expect(result).toEqualData(data);
+			});
+		});
+
 
 	});
 });
--- widgets.js
+++ widgets.js
@@ -1,5 +1,10 @@
 angular.module('app').factory('Widgets', ['$resource',
 	function($resource) {
-		return $resource('url');
+		return $resource('https://api.parse.com/1/classes/widgets', {}, {
+			headers: {
+				'X-Parse-Application-Id': 'kXIwOzlPDyw0Ix1r8z1RlaNyK10CfDO4ww9067ZW',
+				'X-Parse-REST-API-Key': 'WIQ1QYfu1KHbEbRlJp9X7awwwjqqcJu54cOuBJTJ'
+			}
+		});
 	}
 ]);
